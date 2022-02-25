132b3390268972eeb712d4378088271b99626c25
Artem Polyakov <artem.polyakov@corpuls.com>
2022-02-25 14:41:02 +0200

    Fix bug in param parsing

--- dsdwrapper.py
+++ dsdwrapper.py
@@ -49,7 +49,7 @@ def main():
     os.system(commandLine)
 
     # pack to zip archive
-    collect_files_to_zip([outfilename.name,], args.filename)
+    collect_files_to_zip(outfilename.name, [args.filename])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
